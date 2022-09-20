157f6128a129c51122e022218e0ab5fb8fc187de
wawababa <walwaalemayehu@gmail.com>
2022-09-19 19:58:29 -0700

    6-rectangle.py

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Class Rectangle """
+
+
+class Rectangle:
+    """ rectangle """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """ constructor """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter """
+
